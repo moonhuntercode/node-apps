f1c995b80495ec3a1049b1d56dd05cf9b56dd47e
moonhunter <vdcloayza@gmail.com>
2022-04-03 23:54:43 -0400

    probando nuevo metodo de testing

--- test/index.js
+++ test/index.js
@@ -1,15 +1,24 @@
-import http from 'http';
 import express from 'express';
 import assert from 'assert';
-
+import expect from 'chai'
+import request from 'supertest';
 import app from '../lib/index';
 
-describe('Example Node app', () => {
-  it('should return 200', done => {
-    app.get('http://127.0.0.1:3001/', res => {
-      assert.equal(200, res.statusCode);
-      app.close();
-      done();
-    });
+describe('Unit testing the /home route', function() {
+
+  it('should return OK status', function() {
+    return request(app)
+      .get('/home')
+      .then(function(response){
+          assert.equal(response.status, 200)
+      })
   });
+});
+
+it('should return message on rendering', function() {
+  return request(app)
+      .get('/home')
+      .then(function(response){
+          expect(response.text).to.contain('Welcome Home Dude !!');
+      })
 });
